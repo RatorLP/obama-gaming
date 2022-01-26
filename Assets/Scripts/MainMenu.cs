039a85c40561373c69c97b3bf4429f366a8d899f
RatorLP <ratorlpminecraft@gmail.com>
2022-01-26 18:18:39 +0100

    2d project

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*  
+ *  Rico Rotar 
+ *  Main Menu script for obamagaming dungeon crawler game project
+ *  
+ *  2022
+ *  last rev.: 26-JAN-2022-16-28-00
+ */
+
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGame() //loads the next available Scene (buildIndex + 1). To be used for main menu play button only. 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void LoadMenu()
+    public void LoadMenu() //goes back to the main menu without saving. for internal testing only.
     {
-        SceneManager.LoadScene("menu");
+        SceneManager.LoadScene("menu"); 
     }
 
-    public void QuitGame()
+    public void QuitGame() //quits game, to be used on UI quit button
     {
-        Debug.Log("ayo es funktioniert");
+        Debug.Log("used quit button");
         Application.Quit();
     }
 }
