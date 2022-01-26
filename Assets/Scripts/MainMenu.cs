6a212fd9bb3a148125630d663d7dabdbcf5288c1
AntiHenrik <henrik292003@gmail.com>
2022-01-26 18:51:29 +0100

    Kek

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,29 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/*  
- *  Rico Rotar 
- *  Main Menu script for obamagaming dungeon crawler game project
- *  
- *  2022
- *  last rev.: 26-JAN-2022-16-28-00
- */
-
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame() //loads the next available Scene (buildIndex + 1). To be used for main menu play button only. 
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void LoadMenu() //goes back to the main menu without saving. for internal testing only.
+    public void LoadMenu()
     {
-        SceneManager.LoadScene("menu"); 
+        SceneManager.LoadScene("menu");
     }
 
-    public void QuitGame() //quits game, to be used on UI quit button
+    public void QuitGame()
     {
-        Debug.Log("used quit button");
+        Debug.Log("ayo es funktioniert");
         Application.Quit();
     }
 }
