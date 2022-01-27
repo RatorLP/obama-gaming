09c5129c974b73c3541d1d0b74506a729791b16d
RatorLP <ratorlpminecraft@gmail.com>
2022-01-27 12:32:46 +0100

    script changes
    updated main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/* 
+ * Obamagaming dungeon crawler project
+ * Main Menu control script 
+ * 
+ * Rico Rotar
+ * latest revision: 27-JAN-2022-12-32-00
+ */
+
+
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGame() //jumps to the next scene (buildIndex + 1). for main menu play button, might be changed in the future
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void LoadMenu()
+    public void LoadMenu() //goes back to the main menu without saving progress. for debug & testing use only.
     {
         SceneManager.LoadScene("menu");
     }
 
-    public void QuitGame()
+    public void QuitGame() //quits game. to be used by quit button only.
     {
-        Debug.Log("ayo es funktioniert");
+        Debug.Log("used quit button");
         Application.Quit();
     }
 }
