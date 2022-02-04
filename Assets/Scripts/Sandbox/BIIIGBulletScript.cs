a1ef405d67f52fa426c998c9610a091cfc935455
AntiHenrik <henrik292003@gmail.com>
2022-02-04 18:23:54 +0100

    Ranged Enemies added
    The enmies are so far only Turrets but have a Damage variable and 2 different Damage values and speed values.

---
+++ Assets/Scripts/Sandbox/BIIIGBulletScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BIIIGBulletScript : MonoBehaviour
+{
+    public float Damage; // The Variable for Damage done to the player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Damage = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D Col) // Method to chech wether something is hit or not
+    {
+        Destroy(this.gameObject); // destroys the Bullet if it collides woth something
+    }
+}
