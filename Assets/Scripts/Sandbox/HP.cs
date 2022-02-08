3ab99bf168538f7f6a29f4595f11ca5d53ea5093
Ramenuwu <66480551+Ramenuwu@users.noreply.github.com>
2022-02-08 12:22:10 +0100

    Hp bar code

---
+++ Assets/Scripts/Sandbox/HP.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    private Slider slider;
+
+    public float MaxHp = 100; //displays your current level in natural numbers
+    public float CurrHp = 100; //displays the maxium amount of HP you can have
+    public float GainedDmg;
+    private float targetProgress;
+    private float fillspeed; // how fast the bar moves
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>(); //der slider, also die HP bar
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (slider == null)
+        {
+            Debug.Log("No slider found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider == null)
+        {
+            Awake();
+            return;
+        }
+
+        if (GainedDmg != 0) //gerade bekommener damage wird von Hp abgezogen
+        {
+            CurrHp -= GainedDmg;
+        }
+
+        targetProgress = (MaxHp / CurrHp);
+
+        if (slider.value < targetProgress)  //füllt die leiste bis sie den target progress erreichhat,langsam mit jedem update durchlauf
+        {
+            slider.value += fillspeed * Time.deltaTime;
+        }
+
+    }
+
+    //increase Exp
+    public void takeDmg(float addDmg)
+    {
+        GainedDmg += addDmg;
+    }
+}
