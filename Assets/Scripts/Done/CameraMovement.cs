26785d6fd385583525b516c037c8d2708edbe5df
MEdrum <fabian.l.backhaus@gmail.com>
2022-03-22 00:04:10 +0100

    Added comments

--- Assets/Scripts/Done/CameraMovement.cs
+++ Assets/Scripts/Done/CameraMovement.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform target;
-    public float smoothTime = 0.3f;
+    public Transform target; //the player or game object which is followed by the camera
+    public float smoothTime = 0.3f; //the time the camera takes to to catch up to the player
 
-    private Vector3 velocity = Vector3.zero;
+    private Vector3 velocity = Vector3.zero; //resets velocity vector
 
 
     void Update()
     {
         if (target == null)
         {
-            target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            target = GameObject.FindWithTag("Player").GetComponent<Transform>(); //create a reference to the player if it doesn't exist
         }
-        Vector3 goalPos = target.position;
+        Vector3 goalPos = target.position; //set the target position to the player position but keep the camera at its z position
         goalPos.z = transform.position.z;
         transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
     }
